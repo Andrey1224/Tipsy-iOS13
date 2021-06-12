9cfeb47f4d0366562744f0b743350096b54550cb
Andrey1224 <nepod77@gmail.com>
2021-06-11 21:58:38 -0400

    finished with logic

--- Tipsy/Controllers/CalculatorViewController.swift
+++ Tipsy/Controllers/CalculatorViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class CalculatorViewController: UIViewController {
     
+    var tip = 0.10
+    var numberOfPeople = 2
+    
+    var total = 0.0
+    
     
     
     @IBOutlet weak var billTextField: UITextField!
@@ -29,6 +34,19 @@ class CalculatorViewController: UIViewController {
     
     @IBAction func tipChanged(_ sender: UIButton) {
         
+        zeroPctButton.isSelected = false
+        tenPctButton.isSelected = false
+        twentyPctButton.isSelected = false
+        sender.isSelected = true
+        
+        
+        billTextField.endEditing(true)
+        
+        let buttonTitle = sender.currentTitle!
+        let buttonTitlePercent = String(buttonTitle.dropLast())
+        let buttonTitleNumber = Double(buttonTitlePercent)!
+        tip = buttonTitleNumber / 100
+        
         
     }
     
@@ -36,13 +54,26 @@ class CalculatorViewController: UIViewController {
     
     
     @IBAction func stepperValueChanged(_ sender: UIStepper) {
-        
-        
+        splitNumberLabel.text = String(format: "%.0f", sender.value)
+        numberOfPeople = Int(sender.value)
     }
     
     
     @IBAction func calculatePressed(_ sender: UIButton) {
         
+        let bill = billTextField.text!
+        
+        if bill != "" {
+            
+            
+            total = Double(bill)!
+            
+            let result = total * (1 + tip) / Double(numberOfPeople)
+            
+            let resultToDEcimal = String(format: "%.2f", result)
+            
+            print(resultToDEcimal)
+        }
         
     }
     
